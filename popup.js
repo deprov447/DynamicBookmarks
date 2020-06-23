6fb3e3a452fd6aa19b782068e030ffd93fda5d26
deprovX PC <ac.10edu@gmail.com>
2020-06-23 12:30:14 +0530

    worked on popup

--- popup.js
+++ popup.js
@@ -1,23 +1,61 @@
-var toCurrProf=document.querySelector("#toCurrProf");
-var rem=document.querySelector("#all")
+var toCurrProf = document.querySelector("#toCurrProf");
+var rem = document.querySelector("#all");
 var currurl;
-var titl="asdas";
+var titl = "asdas";
 
-chrome.tabs.query({'active': true, 'windowId': chrome.windows.WINDOW_ID_CURRENT},
-function(tabs){
-  currurl=tabs[0].url;
-  titl=tabs[0].title;
-}
+//take query from browser
+chrome.tabs.query(
+  { active: true, windowId: chrome.windows.WINDOW_ID_CURRENT },
+  function (tabs) {
+    currurl = tabs[0].url;
+    titl = tabs[0].title;
+  }
 );
 
-document.querySelector("body").addEventListener("click",function(){
-  document.querySelector("#title").innerHTML=titl;
+//To add bm to Current profile
+toCurrProf.addEventListener("click", function () {
+
+    //create bookmark
+  chrome.bookmarks.create({
+    title: titl,
+    url: currurl,
+  });
+});
+
+var addBM=document.querySelector("#addBM");
+addBM.addEventListener("click",function(){
+  document.querySelector("#p1").parentNode.removeChild(document.querySelector("#p1"));
+  var p2=document.createElement("div")
+  p2.setAttribute("id","p2");
+  p2.innerHTML="<h2>Add this page to?</h2><select id='profile'><option value='' disabled selected>Select</option><option id='createnewpro'>Create New profile</option></select>"
+  
+
+  document.querySelector("#popup_body").appendChild(p2);
+
+
 })
 
-toCurrProf.addEventListener("click",function(){
-  chrome.bookmarks.create({
-    title:titl,
-    url:currurl,
-  })
-  
+function createProf(){
+  window.open("createProfile.html");
+}
+
+function activateProf(x){
+  chrome.storage.sync.get([x], function(result) {
+    console.log('Value currently is ' + result.x.name);
+  });
+
+}
+
+var Curprofile=document.querySelector("#Curprofile");
+Curprofile.addEventListener("change",function(){
+  var x=Curprofile.value;
+  if(x=="createnewpro")
+  {
+    createProf();
+  }
+  else
+  {
+    activateProf(x);
+  }
+  console.log(x);
 })
